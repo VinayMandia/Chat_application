eb49864b418f11cbc9f7426f4e45a2172b9f7901
VinayMandia <109821006+VinayMandia@users.noreply.github.com>
2022-08-17 23:21:28 +0530

    Add files via upload

---
+++ javascript/client.js
@@ -0,0 +1,41 @@
+
+var socket = io('http://localhost:8000', { transports: ['websocket', 'polling', 'flashsocket']});
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+var audio = new Audio('notification.mp3');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const name = prompt("Enter your name");
+    socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'center')
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('left', name =>{
+    append(`${name} has left the chat`, 'center')
+})
